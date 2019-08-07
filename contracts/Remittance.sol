aaf21747f6cd777e3f504bbd9dbef320b450af76
Luke Donato <lukedonato@gmail.com>
2019-08-07 00:41:21 -0400

    remove carol's password, use instance address in hash, use struct

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -6,38 +6,45 @@ import "./Pausable.sol";
 contract Remittance is Pausable {
     using SafeMath for uint;
 
-    mapping (bytes32 => uint) balances;
-
-    event LogDeposit(address owner, uint value);
-    event LogWithdrawal(address withrawer, uint value);
-
-    // offchain
-    function generateHashedCombo(
-        bytes32 carolsPassword,
-        bytes32 bobsPassword,
-        address withdrawerAddress
-    ) public pure returns (bytes32 hashedCombo) {
-         hashedCombo = keccak256(abi.encodePacked(carolsPassword, bobsPassword, withdrawerAddress));
+    struct RemittanceStruct {
+        uint balance;
+        address recipient;
     }
 
-    function depositFunds(bytes32 hashedCombo) payable external returns (bool) {
-        require(hashedCombo > 0);
-        balances[hashedCombo] = balances[hashedCombo].add(msg.value);
+    mapping (bytes32 => RemittanceStruct) remittances;
 
-        emit LogDeposit(msg.sender, msg.value);
+    event LogDeposit(address owner, uint value, bytes32 hashedCombo);
+    event LogWithdrawal(address withrawer, uint value);
+
+    function depositFunds(bytes32 hashedCombo, address withdrawerAddress) payable external returns (bool) {
+        require(hashedCombo > 0);
+        RemittanceStruct storage r = remittances[hashedCombo];
+        require(r.recipient == address(0), "Sorry. Taken.");
+        r.recipient = withdrawerAddress;
+        r.balance = r.balance.add(msg.value);
+
+        emit LogDeposit(msg.sender, msg.value, hashedCombo);
         return true;
     }
 
-    function withdrawFunds(bytes32 carolsPassword, bytes32 bobsPassword) external returns (bool) {
-        bytes32 hashedCombo = keccak256(abi.encodePacked(carolsPassword, bobsPassword, msg.sender));
-        
-        uint secretBalance = balances[hashedCombo];
+    // offchain
+    function generateHashedCombo(
+        bytes32 bobsPassword,
+        address withdrawerAddress
+    ) public pure returns (bytes32 hashedCombo) {
+         hashedCombo = keccak256(abi.encodePacked(bobsPassword, withdrawerAddress, address(this)));
+    }
 
+    function withdrawFunds(bytes32 bobsPassword) external returns (bool) {
+        bytes32 hashedCombo = keccak256(abi.encodePacked(bobsPassword, msg.sender, address(this)));
+        RemittanceStruct storage r = remittances[hashedCombo];
+        
+        require(r.recipient == msg.sender, "This is not for you.");
+        uint secretBalance = r.balance;
         require(secretBalance > 0);
+        r.balance = 0;
 
-        balances[hashedCombo] = 0;
         msg.sender.transfer(secretBalance);
-        
         emit LogWithdrawal(msg.sender, secretBalance);
         return true;
     }
